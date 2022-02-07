fdf0cbbafb76f2f6210178ed6852168ded8a21d3
Andy Roggenbuck <andy_roggenbuck@hotmail.com>
2022-02-07 01:14:25 -0500

    Beginning to merge changes to app_usb.c and .h from example code files

--- src/app_usb.c
+++ src/app_usb.c
@@ -28,12 +28,16 @@
 // *****************************************************************************
 
 #include "app_usb.h"
+#include "common.h"
 
 // *****************************************************************************
 // *****************************************************************************
 // Section: Global Data Definitions
 // *****************************************************************************
 // *****************************************************************************
+
+uint8_t CACHE_ALIGN cdcReadBuffer[APP_USB_READ_BUFFER_SIZE];
+uint8_t CACHE_ALIGN cdcWriteBuffer[APP_USB_WRITE_BUFFER_SIZE];
 
 // *****************************************************************************
 /* Application Data
@@ -58,8 +62,217 @@ APP_USB_DATA app_usbData;
 // *****************************************************************************
 // *****************************************************************************
 
-/* TODO:  Add any necessary callback functions.
-*/
+/*******************************************************
+ * USB CDC Device Events - Application Event Handler
+ *******************************************************/
+
+USB_DEVICE_CDC_EVENT_RESPONSE APP_USB_USBDeviceCDCEventHandler
+(
+    USB_DEVICE_CDC_INDEX index,
+    USB_DEVICE_CDC_EVENT event,
+    void * pData,
+    uintptr_t userData
+)
+{
+    APP_USB_DATA * appDataObject;
+    USB_CDC_CONTROL_LINE_STATE * controlLineStateData;
+    USB_DEVICE_CDC_EVENT_DATA_READ_COMPLETE * eventDataRead;
+    
+    appDataObject = (APP_USB_DATA *)userData;
+
+    switch(event)
+    {
+        case USB_DEVICE_CDC_EVENT_GET_LINE_CODING:
+
+            /* This means the host wants to know the current line
+             * coding. This is a control transfer request. Use the
+             * USB_DEVICE_ControlSend() function to send the data to
+             * host.  
+             */
+
+            USB_DEVICE_ControlSend(appDataObject->deviceHandle,
+                    &appDataObject->getLineCodingData,
+                    sizeof(USB_CDC_LINE_CODING));
+
+            break;
+
+        case USB_DEVICE_CDC_EVENT_SET_LINE_CODING:
+
+            /* This means the host wants to set the line coding.
+             * This is a control transfer request. Use the
+             * USB_DEVICE_ControlReceive() function to receive the
+             * data from the host 
+             */
+
+            USB_DEVICE_ControlReceive(appDataObject->deviceHandle,
+                    &appDataObject->setLineCodingData,
+                    sizeof(USB_CDC_LINE_CODING));
+
+            break;
+
+        case USB_DEVICE_CDC_EVENT_SET_CONTROL_LINE_STATE:
+
+            /* This means the host is setting the control line state.
+             * Read the control line state. We will accept this request
+             * for now. 
+             */
+
+            controlLineStateData = (USB_CDC_CONTROL_LINE_STATE *)pData;
+            appDataObject->controlLineStateData.dtr = controlLineStateData->dtr;
+            appDataObject->controlLineStateData.carrier =
+                    controlLineStateData->carrier;
+
+            USB_DEVICE_ControlStatus(appDataObject->deviceHandle,
+                    USB_DEVICE_CONTROL_STATUS_OK);
+
+            break;
+
+        case USB_DEVICE_CDC_EVENT_SEND_BREAK:
+
+            /* This means that the host is requesting that a break of the
+             * specified duration be sent. Read the break duration 
+             */
+
+            appDataObject->breakData = ((USB_DEVICE_CDC_EVENT_DATA_SEND_BREAK *)
+                    pData)->breakDuration;
+            
+            /* Complete the control transfer by sending a ZLP  */
+            USB_DEVICE_ControlStatus(appDataObject->deviceHandle,
+                    USB_DEVICE_CONTROL_STATUS_OK);
+            
+            break;
+
+        case USB_DEVICE_CDC_EVENT_READ_COMPLETE:
+
+            /* This means that the host has sent some data, store the same */
+            eventDataRead = (USB_DEVICE_CDC_EVENT_DATA_READ_COMPLETE *)pData;
+            
+            /* Notify state machine that a read was completed */
+            
+            
+            /* Update the number of bytes read */
+            appDataObject->numBytesRead = eventDataRead->length;
+            
+            break;
+
+        case USB_DEVICE_CDC_EVENT_CONTROL_TRANSFER_DATA_RECEIVED:
+
+            /* The data stage of the last control transfer is
+             * complete. For now we accept all the data 
+             */
+
+            USB_DEVICE_ControlStatus(appDataObject->deviceHandle,
+                    USB_DEVICE_CONTROL_STATUS_OK);
+            
+            break;
+
+        case USB_DEVICE_CDC_EVENT_CONTROL_TRANSFER_DATA_SENT:
+
+            /* This means the GET LINE CODING function data is valid. We don't
+             * do much with this data in this demo.
+             */
+            
+            break;
+
+        case USB_DEVICE_CDC_EVENT_WRITE_COMPLETE:
+
+            /* This means that the data write got completed and we can schedule
+             * the next read. Notify the state machine by setting a flag. 
+             */
+
+            /* Notify state machine that a write was completed */
+            
+            
+            break;
+
+        default:
+            break;
+    }
+
+    return USB_DEVICE_CDC_EVENT_RESPONSE_NONE;
+}
+
+/***********************************************
+ * Application USB Device Layer Event Handler.
+ ***********************************************/
+void APP_USB_USBDeviceEventHandler 
+(
+    USB_DEVICE_EVENT event, 
+    void * eventData, 
+    uintptr_t context 
+)
+{
+    USB_DEVICE_EVENT_DATA_CONFIGURED *configuredEventData;
+
+    switch(event)
+    {
+        case USB_DEVICE_EVENT_SOF:
+
+            app_usbData.sofEventHasOccurred = true;
+            
+            break;
+
+        case USB_DEVICE_EVENT_RESET:
+
+            /* Update LED to show reset state */
+            USB_LED_Set();
+
+            app_usbData.isConfigured = false;
+
+            break;
+
+        case USB_DEVICE_EVENT_CONFIGURED:
+
+            /* Check the configuration. We only support configuration 1 */
+            configuredEventData = (USB_DEVICE_EVENT_DATA_CONFIGURED*)eventData;
+            
+            if ( configuredEventData->configurationValue == 1)
+            {
+                /* Mark that the device is now configured */
+                
+
+                /* Update LED to show configured state */
+                
+
+                /* Register the CDC Device application event handler here.
+                 * Note how the appData object pointer is passed as the
+                 * user data 
+                 */
+                
+
+            }
+            
+            break;
+
+        case USB_DEVICE_EVENT_POWER_DETECTED:
+
+            /* VBUS was detected. We can attach the device */
+            USB_DEVICE_Attach(app_usbData.deviceHandle);
+            
+            break;
+
+        case USB_DEVICE_EVENT_POWER_REMOVED:
+
+            /* VBUS is not available any more. Detach the device. */
+            USB_DEVICE_Detach(app_usbData.deviceHandle);
+            
+            USB_LED_Set();
+            
+            break;
+
+        case USB_DEVICE_EVENT_SUSPENDED:
+
+            USB_LED_Set();
+            
+            break;
+
+        case USB_DEVICE_EVENT_RESUMED:
+        case USB_DEVICE_EVENT_ERROR:
+        default:
+            
+            break;
+    }
+}
 
 // *****************************************************************************
 // *****************************************************************************

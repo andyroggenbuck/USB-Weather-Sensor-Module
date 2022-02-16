86cd6e09700fa62593bd4d88ac8a68723bb45f4e
Andy Roggenbuck <andy_roggenbuck@hotmail.com>
2022-02-15 23:39:25 -0500

    Changed bulk in endpoint address from 3 to 2. NOW IT WORKS

--- src/config/Trinket_USB_2/usb_device_init_data.c
+++ src/config/Trinket_USB_2/usb_device_init_data.c
@@ -193,7 +193,7 @@ const uint8_t fullSpeedConfigurationDescriptor[]=
 
     0x07,                                                   // Size of this descriptor
     USB_DESCRIPTOR_ENDPOINT,                                // Endpoint Descriptor
-    3 | USB_EP_DIRECTION_IN,                                // EndpointAddress ( EP3 IN )
+    2 | USB_EP_DIRECTION_IN,                                // EndpointAddress ( EP2 IN )
     0x02,                                                   // Attributes type of EP (BULK)
     0x40, 0x00,                                             // Max packet size of this EP
     0x00,                                                   // Interval (in ms)

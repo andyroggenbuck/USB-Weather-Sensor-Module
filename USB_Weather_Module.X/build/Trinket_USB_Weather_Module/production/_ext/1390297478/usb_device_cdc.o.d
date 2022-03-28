c152fc850cbdf88afe85d7143d7e8a149e5ede89
Andy Roggenbuck <andy_roggenbuck@hotmail.com>
2022-03-28 01:16:47 -0400

    "Copy" project USB_Weather_Module created, to see if commits still wipe out adc files
    Sends pressure, temp, humidity values once per second. Responds immediately with "1" when a "t" is received.

---
+++ USB_Weather_Module.X/build/Trinket_USB_Weather_Module/production/_ext/1390297478/usb_device_cdc.o.d
@@ -0,0 +1,274 @@
+build/Trinket_USB_Weather_Module/production/_ext/1390297478/usb_device_cdc.o: \
+ ../src/config/Trinket_USB_2/usb/src/usb_device_cdc.c \
+ ../src/config/Trinket_USB_2/usb/usb_device_cdc.h \
+ ../src/config/Trinket_USB_2/configuration.h \
+ ../src/config/Trinket_USB_2/user.h ../src/config/Trinket_USB_2/device.h \
+ ../src/packs/ATSAMD21E18A_DFP/samd21e18a.h \
+ ../src/packs/CMSIS/CMSIS/Core/Include/core_cm0plus.h \
+ ../src/packs/CMSIS/CMSIS/Core/Include/cmsis_version.h \
+ ../src/packs/CMSIS/CMSIS/Core/Include/cmsis_compiler.h \
+ ../src/packs/CMSIS/CMSIS/Core/Include/cmsis_gcc.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/ac.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/adc.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/dac.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/dmac.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/dsu.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/eic.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/evsys.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/gclk.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/hmatrixb.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/i2s.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/mtb.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/nvmctrl.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/pac.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/pm.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/port.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/ptc.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/rtc.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/sercom.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/sysctrl.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/tc.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/tcc.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/usb.h \
+ ../src/packs/ATSAMD21E18A_DFP/component/wdt.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/ac.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/adc.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/dac.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/dmac.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/dsu.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/eic.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/evsys.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/gclk.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/i2s.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/mtb.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/nvmctrl.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/pac0.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/pac1.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/pac2.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/pm.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/port.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/ptc.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/rtc.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/sbmatrix.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/sercom0.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/sercom1.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/sercom2.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/sercom3.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/sysctrl.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/tc3.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/tc4.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/tc5.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/tcc0.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/tcc1.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/tcc2.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/usb.h \
+ ../src/packs/ATSAMD21E18A_DFP/instance/wdt.h \
+ ../src/packs/ATSAMD21E18A_DFP/pio/samd21e18a.h \
+ ../src/config/Trinket_USB_2/device_cache.h \
+ ../src/config/Trinket_USB_2/toolchain_specifics.h \
+ ../src/packs/CMSIS/CMSIS/Core/Include/cmsis_compiler.h \
+ ../src/config/Trinket_USB_2/usb/usb_common.h \
+ ../src/config/Trinket_USB_2/usb/usb_chapter_9.h \
+ ../src/config/Trinket_USB_2/usb/usb_device.h \
+ ../src/config/Trinket_USB_2/system/system.h \
+ ../src/config/Trinket_USB_2/system/system_common.h \
+ ../src/config/Trinket_USB_2/system/system_module.h \
+ ../src/config/Trinket_USB_2/system/system_common.h \
+ ../src/config/Trinket_USB_2/driver/driver_common.h \
+ ../src/config/Trinket_USB_2/driver/usb/drv_usb.h \
+ ../src/config/Trinket_USB_2/system/int/sys_int.h \
+ ../src/config/Trinket_USB_2/device.h \
+ ../src/config/Trinket_USB_2/system/int/sys_int_mapping.h \
+ ../src/config/Trinket_USB_2/usb/usb_host_client_driver.h \
+ ../src/config/Trinket_USB_2/usb/usb_host.h \
+ ../src/config/Trinket_USB_2/usb/usb_host_hub_interface.h \
+ ../src/config/Trinket_USB_2/usb/src/usb_device_mapping.h \
+ ../src/config/Trinket_USB_2/usb/src/usb_device_function_driver.h \
+ ../src/config/Trinket_USB_2/usb/usb_cdc.h \
+ ../src/config/Trinket_USB_2/usb/src/usb_device_cdc_local.h \
+ ../src/config/Trinket_USB_2/osal/osal.h \
+ ../src/config/Trinket_USB_2/osal/osal_definitions.h \
+ ../src/config/Trinket_USB_2/osal/osal_impl_basic.h \
+ ../src/config/Trinket_USB_2/usb/src/usb_external_dependencies.h
+
+../src/config/Trinket_USB_2/usb/usb_device_cdc.h:
+
+../src/config/Trinket_USB_2/configuration.h:
+
+../src/config/Trinket_USB_2/user.h:
+
+../src/config/Trinket_USB_2/device.h:
+
+../src/packs/ATSAMD21E18A_DFP/samd21e18a.h:
+
+../src/packs/CMSIS/CMSIS/Core/Include/core_cm0plus.h:
+
+../src/packs/CMSIS/CMSIS/Core/Include/cmsis_version.h:
+
+../src/packs/CMSIS/CMSIS/Core/Include/cmsis_compiler.h:
+
+../src/packs/CMSIS/CMSIS/Core/Include/cmsis_gcc.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/ac.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/adc.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/dac.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/dmac.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/dsu.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/eic.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/evsys.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/gclk.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/hmatrixb.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/i2s.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/mtb.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/nvmctrl.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/pac.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/pm.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/port.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/ptc.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/rtc.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/sercom.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/sysctrl.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/tc.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/tcc.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/usb.h:
+
+../src/packs/ATSAMD21E18A_DFP/component/wdt.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/ac.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/adc.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/dac.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/dmac.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/dsu.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/eic.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/evsys.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/gclk.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/i2s.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/mtb.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/nvmctrl.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/pac0.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/pac1.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/pac2.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/pm.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/port.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/ptc.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/rtc.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/sbmatrix.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/sercom0.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/sercom1.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/sercom2.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/sercom3.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/sysctrl.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/tc3.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/tc4.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/tc5.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/tcc0.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/tcc1.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/tcc2.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/usb.h:
+
+../src/packs/ATSAMD21E18A_DFP/instance/wdt.h:
+
+../src/packs/ATSAMD21E18A_DFP/pio/samd21e18a.h:
+
+../src/config/Trinket_USB_2/device_cache.h:
+
+../src/config/Trinket_USB_2/toolchain_specifics.h:
+
+../src/packs/CMSIS/CMSIS/Core/Include/cmsis_compiler.h:
+
+../src/config/Trinket_USB_2/usb/usb_common.h:
+
+../src/config/Trinket_USB_2/usb/usb_chapter_9.h:
+
+../src/config/Trinket_USB_2/usb/usb_device.h:
+
+../src/config/Trinket_USB_2/system/system.h:
+
+../src/config/Trinket_USB_2/system/system_common.h:
+
+../src/config/Trinket_USB_2/system/system_module.h:
+
+../src/config/Trinket_USB_2/system/system_common.h:
+
+../src/config/Trinket_USB_2/driver/driver_common.h:
+
+../src/config/Trinket_USB_2/driver/usb/drv_usb.h:
+
+../src/config/Trinket_USB_2/system/int/sys_int.h:
+
+../src/config/Trinket_USB_2/device.h:
+
+../src/config/Trinket_USB_2/system/int/sys_int_mapping.h:
+
+../src/config/Trinket_USB_2/usb/usb_host_client_driver.h:
+
+../src/config/Trinket_USB_2/usb/usb_host.h:
+
+../src/config/Trinket_USB_2/usb/usb_host_hub_interface.h:
+
+../src/config/Trinket_USB_2/usb/src/usb_device_mapping.h:
+
+../src/config/Trinket_USB_2/usb/src/usb_device_function_driver.h:
+
+../src/config/Trinket_USB_2/usb/usb_cdc.h:
+
+../src/config/Trinket_USB_2/usb/src/usb_device_cdc_local.h:
+
+../src/config/Trinket_USB_2/osal/osal.h:
+
+../src/config/Trinket_USB_2/osal/osal_definitions.h:
+
+../src/config/Trinket_USB_2/osal/osal_impl_basic.h:
+
+../src/config/Trinket_USB_2/usb/src/usb_external_dependencies.h:
---
+++ USB_Weather_Module.X/build/Trinket_USB_Weather_Module/production/_ext/1578018698/xc32_monitor.o.d
@@ -0,0 +1,2 @@
+build/Trinket_USB_Weather_Module/production/_ext/1578018698/xc32_monitor.o: \
+ ../src/config/Trinket_USB_2/stdio/xc32_monitor.c
